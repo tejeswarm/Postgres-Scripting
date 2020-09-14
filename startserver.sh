350b20a3cd30b8553dd006f6ad4cc05e9514c3a2
tejeswarm <44680808+tejeswarm@users.noreply.github.com>
2020-09-14 16:01:45 -0700

    Update startserver.sh

--- startserver.sh
+++ startserver.sh
@@ -1,18 +1,33 @@
 #!/bin/sh
 
 VERSION=$1
-export PATH=$PATH:PGINSTALL.11
+export PATH=$PATH:PGINSTALL.$VERSION
+
+primenv()
+{
+  export PGDATA=/home/temuppar/work/data_prim
+  export PGPORT=5555
+}
+
+secenv()
+{
+  export PGDATA=/home/temuppar/work/data_sec
+  export PGPORT=7777
+}
 
 start()
 {
+  pg_ctl start -D$PGDATA
 }
 
 stop()
 {
+  pg_ctl stop -D$PGDATA -msmart
 }
 
 BuildPrimary()
 {
+  primenv
   initdb -A password --pwfile="/home/temuppar/work/pg/pwd"  -U sa -D$PGDATA --noclean
 }
 
